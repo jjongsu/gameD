c420c398a7d69abf2aad11daf8791dc80ddba28e
jongsu <hjs1002@jei.com>
2023-09-01 13:43:11 +0900

    update

--- src/pages/GameD/Scene/Loading.ts
+++ src/pages/GameD/Scene/Loading.ts
@@ -1,33 +1,33 @@
 import { Scene } from 'phaser';
 import {
-    background,
-    castle,
-    hpContainer,
-    hpFill,
     loadingBlack,
     loadingBox,
     loadingFill,
     preloadBackground,
     preloadTitle,
-    readyBackground,
-    readyChain,
-    readyPlayButton,
+    // background,
+    // castle,
+    // hpContainer,
+    // hpFill,
+    // readyBackground,
+    // readyChain,
+    // readyPlayButton,
 } from '../../../assets/gameD/image/background';
-import { archerBody, archerFoot, archerHandA, archerHandB, archerHead, archerShadow, arrow } from '../../../assets/gameD/image/archer';
-import {
-    btnBattle,
-    btnCoin,
-    btnCoinReward,
-    btnCrystal,
-    btnExit,
-    btnFast1,
-    btnFast2,
-    btnHome,
-    btnPause,
-    btnRetry,
-    levelContainer,
-    settingIcon,
-} from '../../../assets/gameD/image/icon';
+// import { archerBody, archerFoot, archerHandA, archerHandB, archerHead, archerShadow, arrow } from '../../../assets/gameD/image/archer';
+// import {
+//     btnBattle,
+//     btnCoin,
+//     btnCoinReward,
+//     btnCrystal,
+//     btnExit,
+//     btnFast1,
+//     btnFast2,
+//     btnHome,
+//     btnPause,
+//     btnRetry,
+//     levelContainer,
+//     settingIcon,
+// } from '../../../assets/gameD/image/icon';
 
 export default class Loading extends Scene {
     btn?: Phaser.GameObjects.Group;
@@ -42,34 +42,34 @@ export default class Loading extends Scene {
         this.load.image('loadingFill', loadingFill);
         this.load.image('preloadTitle', preloadTitle);
 
-        this.load.image('readyBackground', readyBackground);
-        this.load.image('readyChain', readyChain);
-        this.load.image('readyPlayButton', readyPlayButton);
-        this.load.image('background', background);
-        this.load.image('castle', castle);
+        // this.load.image('readyBackground', readyBackground);
+        // this.load.image('readyChain', readyChain);
+        // this.load.image('readyPlayButton', readyPlayButton);
+        // this.load.image('background', background);
+        // this.load.image('castle', castle);
 
-        this.load.image('arrow', arrow);
-        this.load.image('archerBody', archerBody);
-        this.load.image('archerFoot', archerFoot);
-        this.load.image('archerHandA', archerHandA);
-        this.load.image('archerHandB', archerHandB);
-        this.load.image('archerHead', archerHead);
-        this.load.image('archerShadow', archerShadow);
+        // this.load.image('arrow', arrow);
+        // this.load.image('archerBody', archerBody);
+        // this.load.image('archerFoot', archerFoot);
+        // this.load.image('archerHandA', archerHandA);
+        // this.load.image('archerHandB', archerHandB);
+        // this.load.image('archerHead', archerHead);
+        // this.load.image('archerShadow', archerShadow);
 
-        this.load.image('btnBattle', btnBattle);
-        this.load.image('btnExit', btnExit);
-        this.load.image('btnFast1', btnFast1);
-        this.load.image('btnFast2', btnFast2);
-        this.load.image('btnHome', btnHome);
-        this.load.image('btnPause', btnPause);
-        this.load.image('btnRetry', btnRetry);
-        this.load.image('btnCoinReward', btnCoinReward);
-        this.load.image('btnCoin', btnCoin);
-        this.load.image('btnCrystal', btnCrystal);
-        this.load.image('levelContainer', levelContainer);
-        this.load.image('settingIcon', settingIcon);
-        this.load.image('hpContainer', hpContainer);
-        this.load.image('hpFill', hpFill);
+        // this.load.image('btnBattle', btnBattle);
+        // this.load.image('btnExit', btnExit);
+        // this.load.image('btnFast1', btnFast1);
+        // this.load.image('btnFast2', btnFast2);
+        // this.load.image('btnHome', btnHome);
+        // this.load.image('btnPause', btnPause);
+        // this.load.image('btnRetry', btnRetry);
+        // this.load.image('btnCoinReward', btnCoinReward);
+        // this.load.image('btnCoin', btnCoin);
+        // this.load.image('btnCrystal', btnCrystal);
+        // this.load.image('levelContainer', levelContainer);
+        // this.load.image('settingIcon', settingIcon);
+        // this.load.image('hpContainer', hpContainer);
+        // this.load.image('hpFill', hpFill);
 
         this.load.on('complete', () => this.next());
     }
--- src/pages/GameD/Scene/Preload.ts
+++ src/pages/GameD/Scene/Preload.ts
@@ -1,20 +1,20 @@
 import { Scene } from 'phaser';
-// import { background, castle, hpContainer, hpFill, readyBackground, readyChain, readyPlayButton } from '../../../assets/gameD/image/background';
-// import { archerBody, archerFoot, archerHandA, archerHandB, archerHead, archerShadow, arrow } from '../../../assets/gameD/image/archer';
-// import {
-//     btnBattle,
-//     btnCoin,
-//     btnCoinReward,
-//     btnCrystal,
-//     btnExit,
-//     btnFast1,
-//     btnFast2,
-//     btnHome,
-//     btnPause,
-//     btnRetry,
-//     levelContainer,
-//     settingIcon,
-// } from '../../../assets/gameD/image/icon';
+import { background, castle, hpContainer, hpFill, readyBackground, readyChain, readyPlayButton } from '../../../assets/gameD/image/background';
+import { archerBody, archerFoot, archerHandA, archerHandB, archerHead, archerShadow, arrow } from '../../../assets/gameD/image/archer';
+import {
+    btnBattle,
+    btnCoin,
+    btnCoinReward,
+    btnCrystal,
+    btnExit,
+    btnFast1,
+    btnFast2,
+    btnHome,
+    btnPause,
+    btnRetry,
+    levelContainer,
+    settingIcon,
+} from '../../../assets/gameD/image/icon';
 import * as sprites from '../../../assets/gameD/image/sprite';
 
 export default class Preload extends Scene {
@@ -33,34 +33,34 @@ export default class Preload extends Scene {
     }
 
     preload() {
-        // this.load.image('readyBackground', readyBackground);
-        // this.load.image('readyChain', readyChain);
-        // this.load.image('readyPlayButton', readyPlayButton);
-        // this.load.image('background', background);
-        // this.load.image('castle', castle);
+        this.load.image('readyBackground', readyBackground);
+        this.load.image('readyChain', readyChain);
+        this.load.image('readyPlayButton', readyPlayButton);
+        this.load.image('background', background);
+        this.load.image('castle', castle);
 
-        // this.load.image('arrow', arrow);
-        // this.load.image('archerBody', archerBody);
-        // this.load.image('archerFoot', archerFoot);
-        // this.load.image('archerHandA', archerHandA);
-        // this.load.image('archerHandB', archerHandB);
-        // this.load.image('archerHead', archerHead);
-        // this.load.image('archerShadow', archerShadow);
+        this.load.image('arrow', arrow);
+        this.load.image('archerBody', archerBody);
+        this.load.image('archerFoot', archerFoot);
+        this.load.image('archerHandA', archerHandA);
+        this.load.image('archerHandB', archerHandB);
+        this.load.image('archerHead', archerHead);
+        this.load.image('archerShadow', archerShadow);
 
-        // this.load.image('btnBattle', btnBattle);
-        // this.load.image('btnExit', btnExit);
-        // this.load.image('btnFast1', btnFast1);
-        // this.load.image('btnFast2', btnFast2);
-        // this.load.image('btnHome', btnHome);
-        // this.load.image('btnPause', btnPause);
-        // this.load.image('btnRetry', btnRetry);
-        // this.load.image('btnCoinReward', btnCoinReward);
-        // this.load.image('btnCoin', btnCoin);
-        // this.load.image('btnCrystal', btnCrystal);
-        // this.load.image('levelContainer', levelContainer);
-        // this.load.image('settingIcon', settingIcon);
-        // this.load.image('hpContainer', hpContainer);
-        // this.load.image('hpFill', hpFill);
+        this.load.image('btnBattle', btnBattle);
+        this.load.image('btnExit', btnExit);
+        this.load.image('btnFast1', btnFast1);
+        this.load.image('btnFast2', btnFast2);
+        this.load.image('btnHome', btnHome);
+        this.load.image('btnPause', btnPause);
+        this.load.image('btnRetry', btnRetry);
+        this.load.image('btnCoinReward', btnCoinReward);
+        this.load.image('btnCoin', btnCoin);
+        this.load.image('btnCrystal', btnCrystal);
+        this.load.image('levelContainer', levelContainer);
+        this.load.image('settingIcon', settingIcon);
+        this.load.image('hpContainer', hpContainer);
+        this.load.image('hpFill', hpFill);
 
         this.load.on('progress', (value: number) => this.progressLoading(value));
         this.load.on('complete', () => this.next());
